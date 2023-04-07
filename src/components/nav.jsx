1506e3f35ea8d3392256655e2fbfa6d29953311c
MbuguaGeorge <mbuguageorge026@gmail.com>
2023-04-07 19:50:20 +0300

    updates

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,16 +1,18 @@
 import {Link} from 'react-router-dom';
+import avatar from '../assets/avatar.png';
 
 export default function Nav(){
     return (
         <div className="navigation">
             <nav>
+                <div className="logo"></div>
                 <ul>
                     <Link to="/" style={{textDecoration: 'none', color: '#111'}}><li>Home</li></Link>
                     <Link to="/about" style={{textDecoration: 'none', color: '#111'}}><li>About</li></Link>
                     <Link to="/contact" style={{textDecoration: 'none', color: '#111'}}><li>Contact</li></Link>
                 </ul>
                 <div className="avatar">
-                    <img src='' alt="avatar" />
+                    <img src={avatar} alt="avatar" />
                 </div>
             </nav>
         </div>
